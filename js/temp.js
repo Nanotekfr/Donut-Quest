21518b439abcf7d711882a1306478fc7cc85a37b
Nehred <nehred@hotmail.fr>
2018-04-20 14:57:50 +0200

    scripts mise à jour

--- js/temp.js
+++ js/temp.js
@@ -1,16 +1,22 @@
-dialog {
-  dialogs:[
-    {id:1,sentences:[{id:1,text:"Hello World!"},{id:2,text:"How are you?"}],
-    actions:[{id:1,text:"I'm fine",nextStep:-1},{id:1,text:"Bad...", nextStep:1}]}]
+var talkJSON = `{
+  "id":1,
+  "sentences":[{"id":1,"text":"Hello World!"},{"id":2,"text":"How are you?"}],
+  "actions":[{"id":1,"text":"I'm fine","nextStep":-1},{"id":1,"text":"Bad...", "nextStep":1}]
+}`;
+
+  function talk() {
+  var dialogs = JSON.parse(talkJSON);
+  console.log(dialogs);
+  console.log("sentences:");
+  console.log(dialogs.sentences);
+  for (sentence of dialogs.sentences) {
+    console.log(sentence);
+    document.getElementById("dialog").innerHTML += sentence.text;
   }
-
-var dialog = JSON.parse("dialog");
-
-for sentence of dialogs.sentences {
-  document.getElementById("dialog")innerHTML = dialog.dialogs.sentences[0].text;
-  next
-}
-for sentence of dialogs.actions {
-  document.getElementById("dialog")innerHTML = dialog.dialogs.actions[0].text;
-  next
+  console.log("actions:");
+  console.log(dialogs.actions);
+  for (action of dialogs.actions) {
+    console.log(action);
+    document.getElementById("dialog").innerHTML += "<button>" + action.text + "</button>";
+  }
 }
--- temp.php
+++ temp.php
@@ -3,9 +3,11 @@
 <head>
   <meta charset="utf-8">
   <title></title>
+  <script src="js/temp.js"></script>
 </head>
 <body>
 
+<button onclick="talk()">talk</button>
   <div id="dialog"></div>
 
 </body>
