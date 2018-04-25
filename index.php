a0eb87f280653c672d6d9a0a9f00f7590aad955a
Nehred <nehred@hotmail.fr>
2018-04-25 09:42:35 +0200

    new script 2.0 with animation and sound

--- index.php
+++ index.php
@@ -6,86 +6,22 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
   <section class="character-box">
-    <img id="ned" class="character-image" onclick="nedChangeClass()" src="img/ned_flanders.png" alt="#">
-    <img id="ned_fake" class="character-image fake hidden" src="img/ned_flanders.png" alt="#">
+    <img id="margeTalk" class="character-image" onclick="talk()" src="img/ned_flanders.png" alt="Picture of Ned Flanders from The Simpsons">
+    <img id="margeFake" class="character-image fake hidden" src="img/ned_flanders.png" alt="Picture of Ned Flanders from The Simpsons">
   </section>
 
-  <section class="dialog">
-    <div id="ned_name" class="dialog-character-name hidden">Ned Flanders</div>
-    <div id="ned_greetings" class="dialog-box hidden">
-      <div id="ned_greetings_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_button" class="dialog-button hidden" onclick="homerChangeClass()">Homer</a>
-        <a id="ned_bart_button" class="dialog-button hidden" onclick="bartChangeClass()">Bart</a>
-        <a id="ned_bye_button" class="dialog-button hidden" onclick="byeChangeClass()">Bye</a>
-      </div>
-    </div>
-
-    <div id="ned_greetings_bis" class="dialog-box hidden">
-      <div id="ned_greetings_bis_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_bis_button" class="dialog-button hidden" onclick="homerChangeClass()">Homer</a>
-        <a id="ned_bart_bis_button" class="dialog-button hidden" onclick="bartChangeClass()">Bart</a>
-        <a id="ned_bye_bis_button" class="dialog-button hidden" onclick="byeChangeClass()">Bye</a>
-      </div>
-    </div>
-
-    <div id="ned_bart_reply" class="dialog-box hidden">
-      <div id="ned_bart_reply_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_bart_end_button" class="dialog-button hidden" onclick="bisChangeClass()">OK</a>
-      </div>
-    </div>
-
-    <div id="ned_homer_reply" class="dialog-box hidden">
-      <div id="ned_homer_reply_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_next_button" class="dialog-button hidden" onclick="homernextChangeClass()">></a>
-      </div>
-    </div>
-
-    <div id="ned_homer_reply_next" class="dialog-box hidden">
-      <div id="ned_homer_reply_next_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_yes_button" class="dialog-button hidden" onclick="homeryesChangeClass()">ACCEPT</a>
-        <a id="ned_homer_no_button" class="dialog-button hidden" onclick="homernoChangeClass()">REFUSE</a>
-      </div>
-    </div>
-
-    <div id="ned_homer_reply_yes" class="dialog-box hidden">
-      <div id="ned_homer_reply_yes_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_donut_button" class="dialog-button hidden" onclick="donutChangeClass()">></a>
-      </div>
-    </div>
-
-    <div id="ned_homer_donut" class="dialog-box hidden">
-      <div id="ned_homer_donut_prompt" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_yes_end_button" class="dialog-button hidden" onclick="endChangeClass()">OK</a>
-      </div>
-    </div>
-
-    <div id="ned_homer_reply_no" class="dialog-box hidden">
-      <div id="ned_homer_reply_no_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_homer_no_end_button" class="dialog-button hidden" onclick="bisChangeClass()">OK</a>
-      </div>
-    </div>
-
-    <div id="ned_bye_reply" class="dialog-box hidden">
-      <div id="ned_bye_reply_speech" class="dialog-textarea"></div>
-      <div class="dialog-button-box">
-        <a id="ned_end_dialog_button" class="dialog-button hidden" onclick="endChangeClass()">Ok</a>
-      </div>
+  <section id="dialogBox" class="dialog hidden">
+    <div id="ned_name" class="dialog-character-name">Marge Simpson</div>
+    <div class="dialog-box">
+      <div id="div_dialog" class="dialog-textarea"></div>
+      <div id="div_button" class="dialog-button-box"></div>
     </div>
   </section>
 
-  <a href="kitchen.php">Go To Kitchen</a>
-  <img id="donutimg" class="donutimg hidden" src="img/donut.png">
-
+  <script src="js/howler.min.js"></script>
+  <script src="js/typed.min.js"></script>
   <script src="js/script.js"></script>
-
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,269 +1,114 @@
-var i = 0;
-var count = 0;
-var text = '';
-var selectedtext = '';
+var sfx_blipmale = new Howl({
+src: ['../audio/sfx_blipmale.wav'],
+volume: 0.25,
+loop: true
+});
+var sfx_blipfemale = new Howl({
+src: ['../audio/sfx_blipfemale.wav'],
+volume: 0.25,
+loop: true
+});
 
-var sfx_blipfemale = new Audio('../audio/sfx_blipfemale.wav');
-var sfx_blipmale = new Audio('../audio/sfx_blipmale.wav');
-
-var ned_fake = document.getElementById('ned_fake');
-var ned_name = document.getElementById('ned_name');
-var ned = document.getElementById('ned');
-
-var ned_greetings_bis = document.getElementById('ned_greetings_bis');
-var ned_greetings = document.getElementById('ned_greetings');
-
-var ned_bart_bis_button = document.getElementById('ned_bart_bis_button');
-var ned_bart_button = document.getElementById('ned_bart_button');
-var ned_bart_reply = document.getElementById('ned_bart_reply');
-
-var ned_homer_yes_end_button = document.getElementById('ned_homer_yes_end_button');
-var ned_homer_no_end_button = document.getElementById('ned_homer_no_end_button');
-var ned_homer_donut_button = document.getElementById('ned_homer_donut_button');
-var ned_homer_bis_button = document.getElementById('ned_homer_bis_button');
-var ned_homer_yes_button = document.getElementById('ned_homer_yes_button');
-var ned_homer_no_button = document.getElementById('ned_homer_no_button');
-var ned_homer_reply_yes = document.getElementById('ned_homer_reply_yes');
-var ned_homer_reply_no = document.getElementById('ned_homer_reply_no');
-var ned_homer_button = document.getElementById('ned_homer_button');
-var ned_homer_reply = document.getElementById('ned_homer_reply');
-var ned_homer_donut = document.getElementById('ned_homer_donut');
-
-var ned_bye_bis_button = document.getElementById('ned_bye_bis_button');
-var ned_bye_button = document.getElementById('ned_bye_button');
-var ned_bye_reply = document.getElementById('ned_bye_reply');
-
-var ned_end_dialog_button = document.getElementById('ned_end_dialog_button');
-
-var donutimg = document.getElementById('donutimg');
-
-//0
-
-var ned_greetings_speech = ["Hi diddly ho neighborinos! How may I help you?"];
-function nedChangeClass() {
-  ned.classList.add('hidden');
-  ned_fake.classList.remove('hidden');
-  ned_name.classList.remove('hidden');
-
-  ned_greetings.classList.remove('hidden');
-  selectedtext = ned_greetings_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(nedChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_greetings_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_button.classList.remove('hidden');
-
-    ned_bart_button.classList.remove('hidden');
-
-    ned_bye_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
+var talkJSON = `{
+"id": 1,
+"dialogs": [
+  {
+  "id":1,
+  "sentences":[
+    {"id":1,"text":"Oh hi there neighbor, what can I do for you?"}],
+  "actions":[
+    {"id":1,"text":"HOMER","nextStep": 1},
+    {"id":2,"text":"BART","nextStep": 4},
+    {"id":3,"text":"NOTHING","nextStep": 5}]
+  },
+  {
+  "id":2,
+  "sentences":[
+    {"id":1,"text":"HOMER, he's been out all night... again!"},
+    {"id":2,"text":"By the way. I have this BOX OF DONUTS."},
+    {"id":3,"text":"Could you give it to him? That way you two could meet each other!"}],
+  "actions":[
+    {"id":1,"text":"ACCEPT","nextStep": 2},
+    {"id":2,"text":"REFUSE","nextStep": 3}]
+  },
+  {
+  "id":3,
+  "sentences":[
+    {"id":1,"text":"Great, thank you!"},
+    {"id":2,"text":"He should be in the KITCHEN, come in! The door's open."}],
+  "actions":[
+    {"id":1,"text":"END","nextStep": -1}]
+  },
+  {
+  "id":4,
+  "sentences":[
+    {"id":1,"text":"Oh alright, nevermind then."}],
+  "actions":[
+    {"id":1,"text":"NEXT","nextStep": 6}]
+  },
+  {
+  "id":5,
+  "sentences":[
+    {"id":1,"text":"BART lost his shorts last week, he said some guy ate them..."},
+    {"id":2,"text":"If someone did eat BART’s shorts they’d have a tummy-full of pocket garbage."}],
+  "actions":[
+    {"id":1,"text":"NEXT","nextStep": 6}]
+  },
+  {
+  "id":6,
+  "sentences":[
+    {"id":1,"text":"Ok then, have a nice day!"}],
+  "actions":[
+    {"id":1,"text":"END","nextStep": -1}]
+  },
+  {
+  "id":7,
+  "sentences":[
+    {"id":1,"text":"Anything else?"}],
+  "actions":[
+    {"id":1,"text":"Homer","nextStep": 1},
+    {"id":2,"text":"Bart","nextStep": 4},
+    {"id":3,"text":"Nothing","nextStep": 5}]
   }
-}
+]
+}`;
 
-//0
+var currentDialog = 0;
+var currentSentence = 0;
+var step = JSON.parse(talkJSON);
 
-var ned_greetings_bis_speech = ["Now what can I ding dong diddly do for you?"];
-function bisChangeClass() {
-  ned_name.classList.remove('hidden');
-
-  ned_homer_reply_yes.classList.add('hidden');
-  ned_homer_reply_no.classList.add('hidden');
-  ned_homer_reply.classList.add('hidden');
-  ned_homer_donut.classList.add('hidden');
-  ned_homer_yes_end_button.classList.add('hidden');
-
-  ned_bart_bis_button.classList.add('hidden');
-  ned_bart_reply.classList.add('hidden');
-  ned_bart_button.classList.add('hidden');
-  ned_bart_end_button.classList.add('hidden');
-
-  ned_greetings_bis.classList.remove('hidden');
-
-  donutimg.classList.add('hidden');
-  selectedtext = ned_greetings_bis_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(bisChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_greetings_bis_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_bis_button.classList.remove('hidden');
-
-    ned_bart_bis_button.classList.remove('hidden');
-
-    ned_bye_bis_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
+function talk() {
+  var dialog = step.dialogs[currentDialog];
+  document.getElementById("div_dialog").innerHTML = "";
+  document.getElementById("div_button").innerHTML = "";
+  margeTalk.classList.add('hidden');
+  margeFake.classList.remove('hidden');
+  dialogBox.classList.remove('hidden');
+  sfx_blipfemale.play();
+  if (currentDialog == -1) {
+    currentDialog = 0;
+    currentSentence = 0;
+    margeTalk.classList.remove('hidden');
+    margeFake.classList.add('hidden');
+    dialogBox.classList.add('hidden');
+    sfx_blipfemale.stop();
   }
-}
-
-//0
-
-var ned_homer_reply_speech = ["Homer Simpson, I show him pity, and how does he repay me? With a kick in the kididdlehopper!"];
-function homerChangeClass() {
-  ned_greetings.classList.add('hidden');
-  ned_greetings_bis.classList.add('hidden');
-
-  ned_homer_button.classList.add('hidden');
-  ned_homer_bis_button.classList.add('hidden');
-  ned_homer_reply.classList.remove('hidden');
-
-  ned_bart_button.classList.add('hidden');
-  ned_bart_bis_button.classList.add('hidden');
-
-  ned_bye_button.classList.add('hidden');
-  ned_bye_bis_button.classList.add('hidden');
-  selectedtext = ned_homer_reply_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(homerChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_homer_reply_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_next_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-
-var ned_homer_reply_next_speech = ["Oh by he way, I have this donut for him, could you give it to him for me?"];
-function homernextChangeClass() {
-  ned_homer_reply.classList.add('hidden');
-  ned_homer_next_button.classList.add('hidden');
-  ned_homer_reply_next.classList.remove('hidden');
-  selectedtext = ned_homer_reply_next_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(homernextChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_homer_reply_next_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_yes_button.classList.remove('hidden');
-    ned_homer_no_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-
-var ned_homer_reply_no_speech = ["Oh, nevermind then..."];
-function homernoChangeClass() {
-  ned_homer_yes_button.classList.add('hidden');
-  ned_homer_no_button.classList.add('hidden');
-  ned_homer_reply_next.classList.add('hidden');
-  ned_homer_reply_no.classList.remove('hidden');
-  selectedtext = ned_homer_reply_no_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(homernoChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_homer_reply_no_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_no_end_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-
-var ned_homer_reply_yes_speech = ["Great, here take it! Thanks a lot!"];
-function homeryesChangeClass() {
-  ned_homer_yes_button.classList.add('hidden');
-  ned_homer_no_button.classList.add('hidden');
-  ned_homer_reply_next.classList.add('hidden');
-  ned_homer_reply_yes.classList.remove('hidden');
-  selectedtext = ned_homer_reply_yes_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(homeryesChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_homer_reply_yes_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_homer_donut_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-var ned_homer_donut_prompt = ["You're received a DONUT"];
-function donutChangeClass() {
-  ned_name.classList.add('hidden');
-
-  ned_homer_donut_button.classList.add('hidden');
-  ned_homer_reply_yes.classList.add('hidden');
-  ned_homer_donut.classList.remove('hidden');
-  document.getElementById('ned_homer_donut_prompt').innerHTML = ned_homer_donut_prompt;
-  ned_homer_yes_end_button.classList.remove('hidden');
-  donutimg.classList.remove('hidden');
-  localStorage.setItem('donut', '1');
-}
-
-//0
-
-var ned_bart_reply_speech = ["Bart Simpson? Why, he lives right next door to me. Yeah, when they made him, they did not skimp on the puppy dog tails."];
-function bartChangeClass() {
-  ned_greetings.classList.add('hidden');
-  ned_greetings_bis.classList.add('hidden');
-
-  ned_homer_button.classList.add('hidden');
-  ned_homer_bis_button.classList.add('hidden');
-
-  ned_bart_button.classList.add('hidden');
-  ned_bart_bis_button.classList.add('hidden');
-  ned_bart_reply.classList.remove('hidden');
-
-  ned_bye_button.classList.add('hidden');
-  ned_bye_bis_button.classList.add('hidden');
-  selectedtext = ned_bart_reply_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(bartChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_bart_reply_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_bart_end_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-
-var ned_bye_reply_speech = ["When you meet Jesus, be sure to call him Mr. Christ!"];
-function byeChangeClass() {
-  ned_greetings.classList.add('hidden');
-  ned_greetings_bis.classList.add('hidden');
-
-  ned_homer_button.classList.add('hidden');
-  ned_homer_bis_button.classList.add('hidden');
-
-  ned_bart_button.classList.add('hidden');
-  ned_bart_bis_button.classList.add('hidden');
-
-  ned_bye_button.classList.add('hidden');
-  ned_bye_reply.classList.remove('hidden');
-  selectedtext = ned_bye_reply_speech[count];
-  text = selectedtext.slice(0, ++i);
-  textspeed = setTimeout(byeChangeClass, 0025);
-  sfx_blipmale.play();
-  document.getElementById('ned_bye_reply_speech').innerHTML = text;
-  if (text.length === selectedtext.length) {
-    ned_end_dialog_button.classList.remove('hidden');
-    clearTimeout(textspeed);
-    i = 0;
-  }
-}
-
-//0
-
-function endChangeClass() {
-  ned_homer_yes_end_button.classList.add('hidden');
-  ned_end_dialog_button.classList.add('hidden');
-  ned_homer_donut.classList.add('hidden');
-  ned_bye_reply.classList.add('hidden');
-  ned_name.classList.add('hidden');
-  ned_fake.classList.add('hidden');
-  donutimg.classList.add('hidden');
-  ned.classList.remove('hidden');
+  speechtext = dialog.sentences[currentSentence].text;
+  new Typed('#div_dialog', {
+    strings: [speechtext],
+    typeSpeed: 0,
+    showCursor: false,
+    onComplete: function talk() {
+      sfx_blipfemale.stop();
+      if (currentSentence == (dialog.sentences.length-1)) {
+        for (var i=0;i < dialog.actions.length;i++) {
+          document.getElementById("div_button").innerHTML += "<a class=\"dialog-button\" onclick=\"currentSentence=0; currentDialog=" + dialog.actions[i].nextStep + "; talk()\">" + dialog.actions[i].text + "</a>"
+        }
+      }
+      else {
+        currentSentence++;
+        document.getElementById("div_button").innerHTML += "<a class=\"dialog-button-arrow\" onclick=\"talk()\"> > </a>";
+      }
+    }
+  });
 }
