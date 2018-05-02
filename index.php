bc95b973ef063f683a4907e36a5791e589019729
Nehred <nehred@hotmail.fr>
2018-05-02 16:43:41 +0200

    added sort of tutorial and a puzzle

--- index.php
+++ index.php
@@ -3,27 +3,25 @@
 <head>
   <meta charset="utf-8">
   <title>Donut Quest</title>
-  <link rel="stylesheet" href="css/fa-svg-with-js.css">
+  <link rel="stylesheet" href="css/fontawesome-all.css">
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-  <div id="black_screen"> </div>
+  <div id="blackScreen"></div>
 
   <section class="character-box">
-    <img id="margeTalk" class="character-image" onclick="talk()" src="img/marge_simpson.png" alt="Picture of Marge from The Simpsons">
-    <img id="margeFake" class="character-image fake hidden" src="img/marge_simpson.png" alt="Picture of Marge from The Simpsons">
+    <img id="margeTalk" class="character-image" onclick="talkToMarge()" src="img/marge_simpson.png" alt="Picture of Marge from The Simpsons">
   </section>
 
   <section id="dialogBox" class="dialog hidden">
-    <div id="ned_name" class="dialog-character-name">Marge Simpson</div>
+    <div id="margeNameBox" class="dialog-character-name hidden">Marge Simpson</div>
     <div class="dialog-box">
       <div id="div_sentence" class="dialog-textarea"></div>
       <div id="div_button" class="dialog-button-box"></div>
     </div>
   </section>
 
-  <script src="js/fontawesome-all.js"></script>
   <script src="js/howler.min.js"></script>
   <script src="js/typed.min.js"></script>
   <script src="js/script.js"></script>
